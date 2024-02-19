d8889d0abcc5c7c7263807a155a4793e9daf5d0f
Trevor Hebert <t.hebert73@gmail.com>
2024-02-19 18:16:43 -0500

    Refactoring

--- src/models/Expense.ts
+++ src/models/Expense.ts
@@ -1,7 +1,6 @@
 import { builder } from "../builder";
 import { GraphQLError } from "graphql";
 import { IExpenseData } from "../repositories/ExpenseRepository";
-import PaymentImporter from "../libs/importer/PaymentImporter";
 
 const dateRangeInput = builder.inputType("DateRangeInput", {
   fields: (t) => ({
@@ -148,68 +147,6 @@ builder.mutationField("deleteExpense", (t) =>
     resolve: async (parent, root, args, ctx, info) => {
       try {
         return await ctx.expenseRepository.delete(args.id);
-      } catch (error) {
-        const err = error as Error;
-        throw new GraphQLError(err.message);
-      }
-    },
-  })
-);
-
-builder.objectType("ImportResult", {
-  description: "Resulting imported results information.",
-  fields: (t) => ({
-    status: t.string({
-      resolve: (parent) => {
-        return parent.status;
-      },
-    }),
-
-    total: t.int({
-      resolve: (parent) => {
-        return parent.total
-      }
-    }),
-
-    totalImported: t.int({
-      resolve: (parent) => {
-        return parent.totalImported
-      }
-    })
-  }),
-});
-
-builder.scalarType("File", {
-  serialize: () => {
-    throw new Error("Uploads can only be used as input types");
-  },
-});
-
-export const importType = builder.enumType("ImportType", {
-  values: ["rbc"] as const,
-});
-
-builder.mutationField("parseExpensesImport", (t) =>
-  t.field({
-    authScopes: {
-      loggedIn: true,
-    },
-    type: "ImportResult",
-    args: {
-      file: t.arg({
-        type: "File",
-        required: true,
-      }),
-      type: t.arg({
-        type: importType,
-        required: true,
-      }),
-    },
-    resolve: async (parent, args, ctx) => {
-      try {
-        const paymentImporter = new PaymentImporter(ctx, args);
-
-        return { status: "success", ...await paymentImporter.run()};
       } catch (error) {
         const err = error as Error;
         throw new GraphQLError(err.message);
--- src/models/Payment.ts
+++ src/models/Payment.ts
@@ -1,5 +1,6 @@
 import { GraphQLError } from "graphql";
 import { builder } from "../builder";
+import PaymentImporter from "../libs/importer/PaymentImporter";
 
 builder.prismaObject("Payment", {
   fields: (t) => ({
@@ -117,6 +118,68 @@ builder.mutationField("deletePayment", (t) =>
     resolve: async (parent, root, args, ctx, info) => {
       try {
         return await ctx.paymentRepository.delete(args.id);
+      } catch (error) {
+        const err = error as Error;
+        throw new GraphQLError(err.message);
+      }
+    },
+  })
+);
+
+builder.objectType("ImportResult", {
+  description: "Resulting imported results information.",
+  fields: (t) => ({
+    status: t.string({
+      resolve: (parent) => {
+        return parent.status;
+      },
+    }),
+
+    total: t.int({
+      resolve: (parent) => {
+        return parent.total
+      }
+    }),
+
+    totalImported: t.int({
+      resolve: (parent) => {
+        return parent.totalImported
+      }
+    })
+  }),
+});
+
+builder.scalarType("File", {
+  serialize: () => {
+    throw new Error("Uploads can only be used as input types");
+  },
+});
+
+export const importType = builder.enumType("ImportType", {
+  values: ["rbc"] as const,
+});
+
+builder.mutationField("importPayments", (t) =>
+  t.field({
+    authScopes: {
+      loggedIn: true,
+    },
+    type: "ImportResult",
+    args: {
+      file: t.arg({
+        type: "File",
+        required: true,
+      }),
+      type: t.arg({
+        type: importType,
+        required: true,
+      }),
+    },
+    resolve: async (parent, args, ctx) => {
+      try {
+        const paymentImporter = new PaymentImporter(ctx, args);
+
+        return { status: "success", ...await paymentImporter.run()};
       } catch (error) {
         const err = error as Error;
         throw new GraphQLError(err.message);
